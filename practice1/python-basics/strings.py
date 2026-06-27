1f8b2df1be8ba6458b38c5391d8e90835aae6705
zhantiktaizhan@gmail.com <zhantiktaizhan@gmail.com>
2026-06-27 22:38:01 +0500

    Add strings examples

---
+++ practice1/python-basics/strings.py
@@ -0,0 +1,9 @@
+first_name = "Taizhan"
+last_name = "Zhantemir"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+print(len(full_name))
+print(full_name.upper())
+print(full_name.lower())
